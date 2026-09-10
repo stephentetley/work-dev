340f8efbbea05cf88e27ec392627c6699e1a821a
stephentetley <stephen.tetley@gmail.com>
2026-09-10 19:15:51 +0100

    Updated floc_delta powershell script to be callable from an arbitrary directory

--- asset_utils/scripts/floc_delta.ps1
+++ asset_utils/scripts/floc_delta.ps1
@@ -15,18 +15,28 @@ param (
     probably by an API GET.
 #>
 
-$ZtableFlocdes = "../../../../work/resources/ztables/zt_flocdes_20260205.XLSX"
-$AssetLakePath = "../../../work/2026/asset_lake/asset_lake.ducklake"
+$assetUtilsPath = Join-Path -Path $PSScriptRoot -ChildPath ".."
+$assetUtilsPath = [System.IO.Path]::GetFullPath($assetUtilsPath)
+
+$workPath = $Env:MY_WORK_DIR
+
+if ([string]::IsNullOrEmpty($workPath)) {
+    Write-Host "The environment variable MY_WORK_DIR is not set"
+    exit
+}
+
+$ztableFlocdes = "$workPath/resources/ztables/zt_flocdes_20260205.XLSX"
+$assetLakePath = "$workPath/2026/asset_lake/asset_lake.ducklake"
 
 duckdb -c "set variable floc_delta_worklist = `"$Worklist`";" `
-    -c "set variable ztable_flocdes = `"$ZtableFlocdes`";" `
-    -c "attach 'ducklake:$AssetLakePath' as asset_lake (READ_ONLY);" `
+    -c "set variable ztable_flocdes = `"$ztableFlocdes`";" `
+    -c "attach 'ducklake:$assetLakePath' as asset_lake (READ_ONLY);" `
     -c "attach '$OutFile' as sqlite_db (type sqlite);" `
-    -c ".read ./asset_utils/excel_uploader/sql/create_sqlite_tables.sql" `
-    -c ".read ./asset_utils/floc_delta/sql/01_create_floc_delta_tables.sql" `
-    -c ".read ./asset_utils/floc_delta/sql/02_import_files.sql" `
-    -c ".read ./asset_utils/floc_delta/sql/03_floc_delta_insert_into.sql" `
-    -c ".read ./asset_utils/floc_delta/sql/04_excel_uploader_insert_into.sql" `
+    -c ".read $assetUtilsPath/excel_uploader/sql/create_sqlite_tables.sql" `
+    -c ".read $assetUtilsPath/floc_delta/sql/01_create_floc_delta_tables.sql" `
+    -c ".read $assetUtilsPath/floc_delta/sql/02_import_files.sql" `
+    -c ".read $assetUtilsPath/floc_delta/sql/03_floc_delta_insert_into.sql" `
+    -c ".read $assetUtilsPath/floc_delta/sql/04_excel_uploader_insert_into.sql" `
     -c "detach sqlite_db;" `
     -c "detach asset_lake;"
 
